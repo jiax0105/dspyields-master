e85b572e44ecde1291cca1ac85fe83f24d8a7363
jiaxue <jiauxe@tju.edu.cn>
2023-04-26 17:09:35 +0800

    second commit

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",    ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     include_package_data=True,
 )
